5b32940716ddf1fe1386aabd5a3cee8ef28bf21f
TaOleks <tahtay.aleksey@gmail.com>
2023-05-10 17:15:46 +0200

    Lista zadań - one component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lessonsAngular';
+
+  newTask: string;
+  tasksList: Array<string> = [];
+  tasksDone: Array<string> = [];
+
+
+  add(): void {
+    this.tasksList.push(this.newTask)
+    this.newTask = '';
+    console.log(this.tasksList)
+  }
+
+  remove(task: string) {
+    this.tasksList = this.tasksList.filter(it => it !== task)
+  }
+
+  done(task: string) {
+    this.tasksDone.push(task)
+    this.remove(task)
+    console.log(this.tasksDone)
+  }
+
 }
